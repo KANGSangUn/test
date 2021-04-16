8876f1ed4d73d3abe4199a41d2c69e54e801ab01
sangun <sangun.kang@pepabo.com>
2021-04-16 12:08:21 +0900

    YY

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 const app = express()
   
 app.get('/', function (req, res) {
-  res.send('Hello Worsasdld~ss~~')
+  res.send('Hesdasllo Worsasdld~ss~~')
 });
   
 app.listen(80)
